3c72fc4678ec68d10471b3b1c760502b503691f5
Alfredoooh <alfredopjonas@gmail.com>
2026-07-14 14:22:38 +0100

    update

--- app/src/main/java/com/nexa/app/SplashRouterActivity.kt
+++ app/src/main/java/com/nexa/app/SplashRouterActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.nexa.app.session.SessionManager
 
 class SplashRouterActivity : AppCompatActivity() {
@@ -14,8 +13,6 @@ class SplashRouterActivity : AppCompatActivity() {
     private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // OBRIGATÓRIO antes de super.onCreate() quando o tema é Theme.SplashScreen
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
